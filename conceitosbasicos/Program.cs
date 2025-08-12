d596900755a6d4fe13a4e29797d67261aa7995f2
Lucas Viana - UGB <lucasvfv@gmail.com>
2025-08-12 20:37:06 -0300

    feat: user interation code

--- conceitosbasicos/Program.cs
+++ conceitosbasicos/Program.cs
@@ -1,9 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int idade = 24;
-Console.WriteLine("A idade é " + idade);
+Console.WriteLine("Informe seu nome: ");
+string nome = Console.ReadLine();
 
-if (idade >= 18)
-    Console.WriteLine("Você pode tirar habilitação");
-else
-    Console.WriteLine("Você não pode tirar habilitação");
+Console.WriteLine("Informe seu salário: ");
+decimal salario = decimal.Parse(Console.ReadLine());
+
+Console.WriteLine("Seu nome é " + nome + " e seu salario é " + salario);
+
+
 
